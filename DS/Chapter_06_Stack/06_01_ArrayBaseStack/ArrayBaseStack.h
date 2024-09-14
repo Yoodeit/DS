263669ce8a44a3b591baddc36e841bb2bd2c51d4
Yoodeit <juno8147@gmail.com>
2024-09-14 06:07:08 +0000

    24.09.14.15.07

--- DS/Chapter_06_Stack/06_01_ArrayBaseStack/ArrayBaseStack.h
+++ DS/Chapter_06_Stack/06_01_ArrayBaseStack/ArrayBaseStack.h
@@ -1,6 +1,5 @@
-#ifndef __TAB_STACK_H__
-#define __TAB_STACK_H__
-// 이전 3, 4, 5강에서 배운 것을 바탕으로 혼자 구현해본 헤더파일.
+#ifndef __AB_STACK_H__
+#define __AB_STACK_H__
 
 #define TRUE	1
 #define FALSE	0
@@ -10,11 +9,9 @@ typedef int Data;
 
 typedef struct _arrayStack
 {
-	Data arr[STACK_LEN];
-	int numOfData;  // 생각해보니 굳이 필요 없다. curposition이 곧 데이터개수
-	int curPosition; 
+	Data stackArr[STACK_LEN];
+	int topIndex;
 } ArrayStack;
-
 
 typedef ArrayStack Stack;
 
@@ -23,7 +20,6 @@ int SIsEmpty(Stack * pstack);
 
 void SPush(Stack * pstack, Data data);
 Data SPop(Stack * pstack);
-
 Data SPeek(Stack * pstack);
 
 #endif
--- DS/Chapter_06_Stack/06_01_ArrayBaseStack/Trial_ArrayBaseStack.c
+++ DS/Chapter_06_Stack/06_01_ArrayBaseStack/Trial_ArrayBaseStack.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "Trial_ArrayBaseStack.h"
 
-void StackInit(Stack * pstack);
+void StackInit(Stack * pstack)
 {
 	(pstack->numOfData) = 0; //데이터 수 0으로 초기화
 	(pstack->curPosition) = -1; //현재 위치 -1로 초기화
 }
 
-int SIsEmpty(Stack * pstack);
+int SIsEmpty(Stack * pstack)
 {
 	if(pstack->numOfData == 0) 
 	{
@@ -15,12 +16,12 @@ int SIsEmpty(Stack * pstack);
 		return TRUE;
 	}
 	
-	return False;
+	return FALSE;
 }
 
-void SPush(Stack * pstack, Data data);
+void SPush(Stack * pstack, Data data)
 {
-	if(pstack->numOfData >= LIST_LEN) //지정해둔 스택 길이보다 데이터값이 같거나 더 크면 다 찬거니까
+	if(pstack->numOfData >= STACK_LEN) //지정해둔 스택 길이보다 데이터값이 같거나 더 크면 다 찬거니까
 	{
 		puts("저장이 불가능합니다."); // 저장이 불가능합니다를 출력한다. puts()는 printf()와 비슷하나 자동줄바꿈이 있고 포매팅은 없음.
 		return;
@@ -31,20 +32,21 @@ void SPush(Stack * pstack, Data data);
 	(pstack->numOfData)++; //데이터 수 1 증가
 	
 }
-Data SPop(Stack * pstack);
+
+Data SPop(Stack * pstack)
 {
 	if(!SIsEmpty){
-		Data rdata = pstack->arr[curPosition];
+		Data rdata = pstack->arr[pstack->curPosition];
 		(pstack->curPosition)--;
 		(pstack->numOfData)--;
 		return rdata;
 	}
 }
 
-Data SPeek(Stack * pstack);
+Data SPeek(Stack * pstack)
 {
 	if(!SIsEmpty){
-		Data rdata = pstack->arr[curPosition];
+		Data rdata = pstack->arr[pstack->curPosition];
 		return rdata;
 	}
 }
